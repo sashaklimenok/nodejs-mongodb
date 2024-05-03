84e7f92c8dac795ffb736cec7f88d48585d79dc9
Aliaksandr Klimianok <aliaksandr_klimianok@epam.com>
2024-05-03 10:52:36 +0300

    refactoring

--- src/modules/auth/service/auth.service.interface.ts
+++ src/modules/auth/service/auth.service.interface.ts
@@ -4,5 +4,4 @@ import { AuthSignUpDto } from '../dto/auth.sign-up.dto';
 export interface IAuthService {
   signUpUser: (dto: AuthSignUpDto) => Promise<void>;
   signInUser: (dto: AuthSignInDto) => Promise<void>;
-  isExistsUser: (dto: AuthSignInDto) => Promise<boolean>;
 }
--- src/modules/auth/service/auth.service.ts
+++ src/modules/auth/service/auth.service.ts
@@ -1,19 +1,18 @@
-import { injectable } from 'inversify';
+import { inject, injectable } from 'inversify';
 import { AuthSignInDto } from '../dto/auth.sign-in.dto';
 import { AuthSignUpDto } from '../dto/auth.sign-up.dto';
 import { IAuthService } from './auth.service.interface';
+import { injectKeys } from '../../../shared/constants/injectKeys';
+import { IUserService } from '../../user/service/user.service.interface';
 
 @injectable()
 export class AuthService implements IAuthService {
+  constructor(@inject(injectKeys.IUserService) private userService: IUserService) {}
   async signUpUser(dto: AuthSignUpDto): Promise<void> {
     console.log(dto);
   }
 
   async signInUser(dto: AuthSignInDto): Promise<void> {
     console.log(dto);
-  }
-
-  async isExistsUser(dto: AuthSignInDto): Promise<boolean> {
-    return true;
   }
 }
